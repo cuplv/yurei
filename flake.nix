f1f7bca568ec549d6b190c03d8f30b8182122ef3
buntingun <buntingtonstation@duck.com>
2024-02-29 22:43:45 -0700

    use typelevel nix shell

--- flake.nix
+++ flake.nix
@@ -8,33 +8,18 @@
     flake-utils.follows = "typelevel-nix/flake-utils";
   };
   outputs = { self, nixpkgs, flake-schemas, flake-utils, typelevel-nix }: 
-    let
-      # Helpers for producing system-specific outputs
-      supportedSystems = [ "x86_64-linux" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { 
+    flake-utils.lib.eachDefaultSystem(system:
+      let 
+        pkgs = import nixpkgs {
           inherit system;
           overlays = [ typelevel-nix.overlay ];
         };
-      });
-    in {
-      # Schemas tell Nix about the structure of your flake's outputs
-      schemas = flake-schemas.schemas;
-
-      # Development environments
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          # Pinned packages available in the environment
-          imports = [typelevel-nix.typelevelShell];
-          packages = with pkgs; [
-            curl
-            git
-            wget
-            nixpkgs-fmt
-            # LSP Support
-            nodejs
-          ];
+      in
+      {
+        devShell = pkgs.devshell.mkShell {
+          imports = [ typelevel-nix.typelevelShell ];
+          name = "yurei shell";
         };
-      });
-    };
+      }
+    );
 }
